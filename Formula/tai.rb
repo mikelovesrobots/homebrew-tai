f2bc14427308691e294acedc263d12104e84d498
Mike Judge <mike@substantial.com>
2023-03-22 23:46:01 -0700

    Add pip requirements.txt dependencies as resources

--- Formula/tai.rb
+++ Formula/tai.rb
@@ -11,6 +11,17 @@ class Tai < Formula
   license "MIT" # Update with the appropriate license for your project.
   version "0.1.0"
 
+  # requirements.txt dependencies
+  resource "openai" do
+    url "https://files.pythonhosted.org/packages/a9/94/3d4755c8126eeb4b3469d8b9aff5600085f6d7de691da1ac04c79c24dc48/openai-0.27.2.tar.gz"
+    sha256 "5869fdfa34b0ec66c39afa22f4a0fb83a135dff81f6505f52834c6ab3113f762"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+  end
+
   def install
     virtualenv_install_with_resources
   end
@@ -20,5 +31,3 @@ class Tai < Formula
     assert_match "PUT_EXPECTED_OUTPUT_HERE", shell_output("#{bin}/tai PUT_TEST_COMMAND_HERE")
   end
 end
-
-
